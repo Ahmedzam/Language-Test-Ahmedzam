e99eaf330fab3549374693a9fb866690ff5a3bd2
Tom Dalbo <dalbo@us.ibm.com>
2015-09-09 18:25:31 -0400

    Added a custom servlet Filter to add the CORS allow to all request headers

---
+++ framework-ega-infra/src/main/java/com/ibm/watson/app/common/util/rest/CORSFilter.java
@@ -0,0 +1,28 @@
+package com.ibm.watson.app.common.util.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+	@Override
+	public void destroy() {}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	     HttpServletResponse sresponse = (HttpServletResponse) response;
+	     sresponse.setHeader("Access-Control-Allow-Origin", "*");
+	     chain.doFilter(request, response);
+	}
+	
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {}
+
+}
